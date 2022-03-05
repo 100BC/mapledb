28c8c1334ea10fc48763bbc67187bbb94adf37c4
100BC <73491141+100BC@users.noreply.github.com>
2022-03-04 18:36:25 -0700

    Admin home page styling

--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -13,32 +13,33 @@ const Home = () => {
       <h1>Welcome</h1>
       <hr />
       <h2>What would you like to do?</h2>
+      <Environment />
       <section className={styles.section}>
-        <Environment />
+        <h2>Musician</h2>
         <div className={styles.row}>
           <Link href="/submit/musician">
             <a>Submit Musician</a>
           </Link>
-          <Link href="/submit/music">
-            <a>Submit Music</a>
-          </Link>
-        </div>
-        <div className={styles.row}>
           <Link href="/update/musician">
             <a>Edit Musician</a>
           </Link>
-          <Link href="/update/music">
-            <a>Edit Music</a>
-          </Link>
-        </div>
-        <div className={styles.row}>
           <Link href="/delete/musician">
             <a>Delete Musician</a>
+          </Link>
+        </div>
+        <h2>Music</h2>
+        <div className={styles.row}>
+          <Link href="/submit/music">
+            <a>Submit Music</a>
+          </Link>
+          <Link href="/update/music">
+            <a>Edit Music</a>
           </Link>
           <Link href="/delete/music">
             <a>Delete Music</a>
           </Link>
         </div>
+
         <button
           type="button"
           className={styles.signOut}
