29a3cf04e29f06633e8041be9abb12464572ad4a
100BC <73491141+100BC@users.noreply.github.com>
2022-03-06 22:29:02 -0700

    Remove Admin Meta Tags

--- apps/admin/src/components/Layout/defaults.ts
+++ apps/admin/src/components/Layout/defaults.ts
@@ -1,5 +1,2 @@
 // META TAGS
 export const META_TITLE = 'Admin';
-export const SITE_NAME = '';
-export const DOMAIN_NAME = 'https://admin.mooseical.com';
-export const META_DESCRIPTION = '';
--- apps/admin/src/components/Layout/index.tsx
+++ apps/admin/src/components/Layout/index.tsx
@@ -3,12 +3,7 @@ import React, { ReactNode } from 'react';
 import { XOR } from '@mooseical/generics';
 import MetaTags, { MetaTagProps } from '@mooseical/shared/components/MetaTags';
 import Header from '@components/Header';
-import {
-  DOMAIN_NAME,
-  META_DESCRIPTION,
-  META_TITLE,
-  SITE_NAME,
-} from './defaults';
+import { META_TITLE } from './defaults';
 
 export * from './defaults';
 
@@ -21,8 +16,8 @@ type Props = {
 const Layout = (props: Props) => {
   const {
     title = META_TITLE,
-    description = META_DESCRIPTION,
-    canonicalUrl = DOMAIN_NAME,
+    description,
+    canonicalUrl,
     canonicalUrlPath = '',
     className,
     children,
@@ -31,14 +26,7 @@ const Layout = (props: Props) => {
 
   return (
     <>
-      <MetaTags
-        title={title}
-        description={description}
-        canonicalUrl={[canonicalUrl, canonicalUrlPath].join('')}
-        siteName={SITE_NAME}
-        noIndex
-        {...ogp}
-      />
+      <MetaTags title={title} siteName="Admin" noIndex {...ogp} />
       <Header />
       <main className={className}>{children}</main>
     </>
