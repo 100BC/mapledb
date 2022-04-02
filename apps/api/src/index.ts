d57450fb9a545981182df387df3d4aaa597b1e7e
100BC <73491141+100BC@users.noreply.github.com>
2022-04-02 03:09:38 -0600

    Update for reverse proxy

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,9 +2,9 @@ import fastify from '@server';
 import { IS_DEV } from '@utils/constants';
 
 /* INIT */
-fastify.get('/', async (_request, reply) => {
+fastify.get('/*', async (_request, reply) => {
   if (IS_DEV) return reply.redirect(307, '/altair');
-  return reply.code(404).send({ message: 'Not Found' });
+  return reply.callNotFound();
 });
 
 fastify.listen(3002, (err, _address) => {
