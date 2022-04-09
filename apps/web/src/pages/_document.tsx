968e423fd706f170affa22af3f47041ddd281bdb
100BC <73491141+100BC@users.noreply.github.com>
2022-04-08 22:00:11 -0600

    Font fixes

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -30,12 +30,6 @@ class MyDocument extends Document {
             href="/apple-touch-icon.png"
           />
           {/* GOOGLE FONT */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Cherry+Swash:wght@700&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400&display=swap"
             rel="stylesheet"
