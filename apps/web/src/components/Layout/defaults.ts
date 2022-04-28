ab5680dacaf700b09cd54049342a1d39a55e6fa0
100BC <73491141+100BC@users.noreply.github.com>
2022-04-28 10:20:29 -0600

    Fix url endpoints

--- apps/web/src/components/Layout/defaults.ts
+++ apps/web/src/components/Layout/defaults.ts
@@ -1,6 +1,6 @@
 // META TAGS
 export const META_TITLE = 'MapleDB | Canadian Music Database';
 export const SITE_NAME = 'MapleDB';
-export const DOMAIN_NAME = 'https://www.mapledb.com';
+export const DOMAIN_NAME = 'https://www.mapledb.ca';
 export const META_DESCRIPTION =
   'Discover new underground Canadian musicians and music from your favourite genres.';
